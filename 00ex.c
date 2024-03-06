0128dbd52a884345711b88c3b54f536a769b7dbf
Megami.ay <megami.ay@gmail.com>
2024-03-05 21:15:37 -0300

    Create 00ex.c

---
+++ 00ex.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// primeiro exercicio de C ANSI
+
+int main (void){ 
+	printf("Name: Laiz Detros\n");
+	printf("Years: 18 anos\n");
+	printf("Street: expedicionario holtz\n");
+	printf("CPF: 123.456.789-00\n");
+	printf("Phone: +55 (47) 99177-2731\n");
+	printf("Email: laizbdetros@gmail.com\n");
+	printf("CEP: 89237-505");
+
+	return 0;
+}
