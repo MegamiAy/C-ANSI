e8b873e11162ae8f37d7286c30482ae5e73c1e78
Megami.ay <megami.ay@gmail.com>
2024-03-05 21:28:19 -0300

    Create 01ex.c

---
+++ 01ex.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+	// declarcao
+	int idade;
+	float altura;
+	char letra;
+	
+	// insercao
+	idade = 18;
+	altura = 1.65;
+	letra = 'L';
+	
+	// imprimir em tela
+	printf("Idade = %i\n", idade);
+	printf("Altura = %f\nLetra = %c", altura, letra);
+		
+	
+	return(0);
+}
