53593f42b0132f52291e87f3153bc38c81660ca6
megami <megami.ay@gmail.com>
2024-03-05 23:58:51 -0300

    atividade 1

--- Atividades/A00.c
+++ Atividades/A00.c
@@ -1,7 +1,24 @@
+/*
+	Transformar Celsius para Farenheit
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main (void){
+	// Quais sao as variaveis
+	int celsius, farenheit;
+	
+	// Qual e a entrada de dados?
+	printf("Digite a temperatura em celsius: ");
+	scanf("%i", &celsius);
+	fflush(stdin);
+	
+	// Qual o processamento dos dados?
+	farenheit=celsius * 1.8 + 32;
+	
+	// Qual e a saida de dados?
+	printf("%i ºC em Farenheit sao %i ºF", celsius, farenheit);	
 	
 	
 	return(0);
