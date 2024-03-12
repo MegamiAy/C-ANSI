29fbc49deadab021526b975cf5d32b334b5e6de0
Megami.ay <megami.ay@gmail.com>
2024-03-12 19:35:33 -0300

    Create 04ex.c

---
+++ Exercícios/04ex.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+	
+	printf(" -------------------------------\n");
+	printf("| NOME: Sei La                  |\n");
+	printf("| EMAIL: seila@gmail.com        |\n"); 
+	printf("| TELEFONE: +55 33 9999-9999    |\n");
+	printf(" -------------------------------");
+
+
+return (0);
+}
