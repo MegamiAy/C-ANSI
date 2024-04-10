2e05e6fa3a9e212e18b2d2dea3aa952866d7c5eb
Megami.ay <megami.ay@gmail.com>
2024-04-09 21:06:51 -0300

    Update 22.2ex.c

--- Exercícios/22.2ex.c
+++ Exercícios/22.2ex.c
@@ -1,3 +1,14 @@
+// Faça um programa que leia a idade de quatro alunos e apresente a mensagem conforme instruções:
+
+	// a. Se a média de idade dos alunos é inferior de 25, apresentar a mensagem "Turma Jovem";
+
+	// b. Se a média de idade dos alunos é entre 25 e 40, apresentar a mensagem "Turma Adulta";
+
+	// c. Se a média de idade dos alunos é acima de 40 anos, apresentar a mensagem "Turma Idosa".
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int main (void){
 	int n1, n2, n3;
 	
