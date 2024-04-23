e155020c92adf4779e19bb81e60fe446895a25ad
Megami.ay <megami.ay@gmail.com>
2024-04-23 20:07:23 -0300

    Create 26ex.c

---
+++ Exercícios/26ex.c
@@ -0,0 +1,30 @@
+/* de 1 a 10 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+	int n;
+	
+	for(n=1; n<=10; n++){
+		printf("%i \n", n);
+	}
+
+	// ou
+	
+//	n=1;
+//	while(n<=10){
+//		printf("%i \n", n);
+//		n++
+//	}
+	
+	// ou
+	
+//	n=1;
+//	do {
+//		printf("%i \n", n);
+//		n++;
+//	} while(n<=10);
+	
+	return 0;
+}
